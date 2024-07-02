7bd89163f95c1abdd1bff4a37d585b60a3d0b504
ProjectZ <31702802+ZoeZ-Code@users.noreply.github.com>
2024-07-02 22:37:06 +0000

    exit codes

--- src/Ch01/01_02e/CodeDemo.cpp
+++ src/Ch01/01_02e/CodeDemo.cpp
@@ -8,5 +8,15 @@ int main(){
     std::cout << "Hi There!" << std::endl;
 
     std::cout << std::endl << std::endl;
-    return (0);
+    return (0); 
+    // Exit code
+    // 0: Success
+    // 1: General Error
+    // 2: Misuse of Shell Built-ins
+    // 126: Command Invoked Cannot Execute
+    // 127: Command Not Found
+    // 128: Invalid Argument to Exit
+    // 130: Script Terminated by Control-C (SIGINT)
+    // 137: Termination by SIGKILL
+    // 139: Segmentation Fault (SIGSEGV)
 }
